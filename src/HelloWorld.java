1c1ba78852b4ca541fbdbd3e7c27599a018e12d8
Mharis13 <mario12ban@gmail.com>
2023-06-21 22:23:59 +0200

    HELLOWORLDSCANNER

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String []args) throws Exception{
-        System.out.println("HELLOWORLD!!");
+        Scanner scannerName = new Scanner(System.in);
+
+        System.out.print("Type your name: ");
+        String userName= scannerName.nextLine();
+        System.out.println("Hi, your name is: " + userName);
+
+        scannerName.close();
     }
 }
