bcefc52c4f800f01086f46a19164a4a431d1070c
Mharis13 <mario12ban@gmail.com>
2023-06-25 11:41:10 +0200

    DecirTuEdad

--- src/DecirtuEdad.java
+++ src/DecirtuEdad.java
@@ -5,12 +5,25 @@ public class DecirtuEdad{
         Scanner Age = new Scanner(System.in);
         System.out.print("Tell me your age: ");
         int Age2  = Age.nextInt();
-        if (Age2<18) {
-            System.out.println("Your age is: "+ Age2 );
+        Comprobacion(Age2);
+       
+    Age.close();
+ }
+ private static boolean VerdadMentira(int age3) {
+    return age3 > 18;
+
+ }
+ private static void Comprobacion(int age4){
+
+    boolean isAdult=VerdadMentira(age4);
+    
+        if (!isAdult) {
+            System.out.println("Your age is: "+ age4 );
             System.out.println("You are a minor");
         }else{
-            System.out.println("You are not a minor and your age is: "+ Age2);
+            System.out.println("You are not a minor and your age is: "+ age4);
         }
-    Age.close();
+
+
  }
 }
